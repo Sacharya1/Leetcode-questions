5b57546bf8c1b137082f538f6ae8932830b58c7d
Sacharya1 <32911273+Sacharya1@users.noreply.github.com>
2021-07-05 21:31:22 -0700

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -2,8 +2,7 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myDict=dict()
-        
+        myDict=dict()      
         for i in range(len(nums)):
             if (target-nums[i]) in myDict:
                 return [myDict[(target-nums[i])],i]
