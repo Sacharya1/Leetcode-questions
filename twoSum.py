5b96fb4aab7ca3e1684e840c4e4b93e3d6c60e87
Sacharya1 <32911273+Sacharya1@users.noreply.github.com>
2021-07-05 21:29:39 -0700

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+#1. Two Sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict=dict()
+        
+        for i in range(len(nums)):
+            #if nums[i] not in myDict:
+
+            if (target-nums[i]) in myDict:
+                return [myDict[(target-nums[i])],i]
+            else:
+                myDict[nums[i]]=i
